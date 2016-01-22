db7f9817ec166711547c6a8191c0d6ce3e6af2f0
Chris Kuh <kuh_chris@yahoo.com>
2016-01-22 14:33:59 -0800

    [feature] setup db

---
+++ server/db/db.js
@@ -0,0 +1,33 @@
+var Sequelize = require("sequelize");
+
+var sequelize = null;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  // the application is executed on the local machine ... use mysql
+  sequelize = new Sequelize('plant', 'root', null)
+}
+
+var User = sequelize.define("User", {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  profilePicture: Sequelize.BLOB('long'),
+},{
+  createdAt: false,
+  updatedAt: false
+});
+
+var Device = sequelize.define("Device", {
+  apiKey: Sequelize.STRING
+});
+
+
+User.hasMany(Device, {as: 'device'})
+
+User.sync();
+Device.sync();
+
+exports.User = User;
+exports.Device = Device;
+exports.sequelize = sequelize;
